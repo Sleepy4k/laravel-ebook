fa757c0c804d08b505433025ec287e9a4a139691
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-10-17 19:42:31 +0700

    feat: resource

---
+++ app/Http/Resources/SiswaResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SiswaResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "nama" => $this->nama,
+            "jenis_kelamin" => $this->kelamin,
+            "umur" => $this->umur . ' Tahun'
+        ];
+    }
+}
