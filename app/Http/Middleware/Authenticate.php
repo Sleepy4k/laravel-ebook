2ec919ea7218ea2100b24730a3ff9683430f011d
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-10-19 16:44:15 +0700

    feat: middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Middleware;
 
+use App\Traits\ApiRespons;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+    use ApiRespons;
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
@@ -14,7 +17,16 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
+        if ($request->expectsJson()) {
+            $this->createResponse(401, 'Unauthenticated Access',
+                [
+                    'error' => 'Please authenticate your self as an user'
+                ],
+                [
+                    $request->url()
+                ]
+            );
+        } else {
             return route('login');
         }
     }
