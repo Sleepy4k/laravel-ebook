18546c60fed58b60b30108150bee675dd99951a7
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-10-22 19:32:48 +0700

    feat: api controller

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\ApiRespons;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class ApiController extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ApiRespons;
+    
+    /**
+     * Handler try catch error.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    private function catchError($error, $route)
+    {
+        return $this->createResponse(500, 'Server Error',
+            [
+                'error' => $error->getMessage()
+            ],
+            [
+                $route
+            ]
+        );
+    }
+}
