3d1024d8d158aa0a8f0c8975ceeece340ab3ffd9
Sleepy4k <sarahpalastrin@gmail.com>
2022-10-31 15:42:49 +0700

    feat: resource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'Nama Pengguna' => $this->nama,
+            'Nama Alias' => $this->username,
+            'Bergabung Pada' => $this->created_at->format('d-m-Y')
+        ];
+    }
+}
