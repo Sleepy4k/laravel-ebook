ccadfd7483508e942d9a78c18a61cd568d2c9ba1
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-11-05 16:39:57 +0700

    feat: providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,8 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Json Wrap
+        // Resource
         JsonResource::withoutWrapping();
+        ResourceCollection::withoutWrapping();
 
         // Paginate
         Paginator::useBootstrap();
